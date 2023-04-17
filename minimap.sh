fae455fc07c0608eedbe59143feb4b4697352f6a
Bhavika <114206695+bhavikakumar23@users.noreply.github.com>
2023-04-17 23:41:20 +1000

    Update minimap.sh

--- minimap.sh
+++ minimap.sh
@@ -1,24 +1,24 @@
 # Running minimap2 on samples
 
-# minimap2 -ax splice -uf -k14 ref.fa direct-rna.fq > aln.sam
+minimap2 -ax splice -uf -k14 ref.fa direct-rna.fq > aln.sam
 
-#ref=/home/u7442965/ash_nvme_direct_rna/genomes/myrtle_rust/GCA_023105745.1_APSI_hap1_v2_genomic.fa
-#file_3=/home/u7442965/ash_nvme_direct_rna/all_fastq/apsi_r3.fastq
-#out_3=/home/u7442965/ash_nvme_direct_rna/mapping/bk_mapping/apsi_r3.sam
+ref=/home/u7442965/ash_nvme_direct_rna/genomes/myrtle_rust/GCA_023105745.1_APSI_hap1_v2_genomic.fa
+file_3=/home/u7442965/ash_nvme_direct_rna/all_fastq/apsi_r3.fastq
+out_3=/home/u7442965/ash_nvme_direct_rna/mapping/bk_mapping/apsi_r3.sam
 
-#minimap2 -ax splice -uf -k14 $ref $file_3 > $out_3
+minimap2 -ax splice -uf -k14 $ref $file_3 > $out_3
 
 # converting sam to bam
-#samtools view -S -b apsi_r2.sam > apsi_r2.bam
+samtools view -S -b apsi_r2.sam > apsi_r2.bam
 
 # sorting a bam file
-# samtools sort apsi_r2.bam -o apsi_r2_sorted.bam
+samtools sort apsi_r2.bam -o apsi_r2_sorted.bam
 
 # indexing
-# samtools index apsi_r2_sorted.bam
+samtools index apsi_r2_sorted.bam
 
 # NanoStat on samples
-# NanoStat --bam apsi_r2_sorted.bam > apsi_r2_sorted.txt 
+NanoStat --bam apsi_r2_sorted.bam > apsi_r2_sorted.txt 
 
 ref=/home/u7442965/ash_nvme_direct_rna/genomes/p_graminis/Puccinia_graminis_transcriptome.fa
 file=/home/u7442965/ash_nvme_direct_rna/genomes/p_graminis/apsi_r1_induro_genes_1148159.fa
