f1f8caeb7db24a361412a7682ce284ab97916215
Bhavika <114206695+bhavikakumar23@users.noreply.github.com>
2023-04-17 23:40:03 +1000

    Update basecalling.sh

--- basecalling.sh
+++ basecalling.sh
@@ -1,4 +1,10 @@
 
+#guppy_basecaller -i <input path> -s <save path> -c <config file> [options]
+#rna_r9.4.1_70bps_hac
+#added device auto because we are using GPU
+#nuhup- runs the script in the background
+#nohup guppy_basecaller -i $in -s $out -c rna_r9.4.1_70bps_hac.cfg --disable_qscore_filtering --device auto --records_per_fastq 0 --recur>
+
 guppy=/home/u7442965/ash_nvme_direct_rna/ont-guppy/bin/guppy_basecaller
 
 in=/home/u7442965/ash_nvme_direct_rna/raw_fast5/apsi_r3_SQK-RNA002_FLO-MIN106D
@@ -7,10 +13,6 @@ out=/home/u7442965/ash_nvme_direct_rna/raw_fastq/apsi_r3_SQK-RNA002_FLO-MIN106D_
 nohup $guppy -i $in -s $out -c rna_r9.4.1_70bps_hac.cfg --disable_qscore_filtering --device auto --records_per_fastq 0 --recursive &
 
 
-#guppy_basecaller -i <input path> -s <save path> -c <config file> [options]
-#rna_r9.4.1_70bps_hac
-#added device auto because we are using GPU
-#nuhup- runs the script in the background
-#nohup guppy_basecaller -i $in -s $out -c rna_r9.4.1_70bps_hac.cfg --disable_qscore_filtering --device auto --records_per_fastq 0 --recur>
+
 
 
